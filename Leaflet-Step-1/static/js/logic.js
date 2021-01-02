8f76178bf3551c5ea0502ce655fd7c23d37166d6
Joshua Sohan <joshuasohan@Joshuas-MBP.fios-router.home>
2021-01-02 18:41:53 -0500

    testing

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -9,7 +9,7 @@ var myMap = L.map("map", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.streets",
-    accessToken: "API_Key"
+    accessToken: "API_KeAIzaSyA5YfGcsz-7LYxNN4jOZrHanyYhCs0D9ac"
   }).addTo(myMap);
 
   // Geojson data
